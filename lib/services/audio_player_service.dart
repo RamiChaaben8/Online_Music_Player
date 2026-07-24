f6ccb0d1c63881ff2c0fb3bff99fa0b3e49c7cad
ramichaaben8 <rami.chaaben@iit.ens.tn>
2026-07-24 12:04:15 +0100

    ver beta 2.0

--- lib/services/audio_player_service.dart
+++ lib/services/audio_player_service.dart
@@ -176,6 +176,9 @@ class AudioPlayerService {
         AudioSource.uri(
           Uri.parse(streamUrl),
           tag: mediaItem,
+          headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+          },
         ),
       );
 
